a75a3413b92e762b08e8d76e9f895f59c5fbfa7a
wPlachno <wjplachno@gmail.com>
2024-08-24 16:02:25 -0700

    08/24/24 - Updating utilities (wplachno)

--- wcutil.py
+++ wcutil.py
@@ -6,12 +6,24 @@ Woodchipper Utilities
 An assortment of helpful functions and classes.
 
 Includes:
-- FlagFarm class: A simple dictionary wrapper for boolean flags
-- Debug class: A set of debugging tools
+
+- Classes:_________________________________________________________
+- -- FlagFarm class: A simple dictionary wrapper for boolean flags
+- -- Debug class: A set of debugging tools
+
+- Globals:_________________________________________________________
+
+- Functions:_______________________________________________________
+- -- valid_directory_at: Returns whether the path is a directory,
+        safely defaulting to False.
+- -- text_has_paths: Checks whether the given string has any of the
+        common path delimiters.
+- -- tail_matches_token: Checks whether the end of the given string
+        matches the given token. Useful for checking file types.
+- -- time_stamp:
 """
 import pathlib
 from datetime import datetime
-
 
 class FlagFarm:
     def __init__(self, list_of_flag_keys):
@@ -51,7 +63,7 @@ class Debug:
         self.is_active = active
         self.handlers = [message_handler]
 
-    def scribe(self, *message):
+    def scribe(self, message):
         if self.is_active:
             for handler in self.handlers:
                 handler(message)
@@ -116,6 +128,18 @@ def time_stamp(time=None):
         return datetime(time).strftime(preferred_time_format)
     return datetime.now().strftime(preferred_time_format)
 
+onSynonyms = list(("on","true","1","yes","t","y","+","active","positive", "a", "p"))
+def str2Bool(rawText:str):
+    text = rawText.lower()
+    if text in onSynonyms:
+        return True
+    return False
+
+def bool2Str(value:bool, include_color:bool=False):
+    pretext = '\033[0;32m' if value else '\033[0;31m'
+    text = "on" if value else "off"
+    return pretext+text+'\033[0m' if include_color else text
+
 def run_on_sorted_list(target_list, function_given_item):
     """
     Sorts the list, then runs the function on each item.
@@ -152,6 +176,9 @@ class WoodChipperFile:
         self.path = pathlib.Path(filePath)
         self.name = self.path.name
         self.text = list(())
+        if not self.path.exists():
+            file = open(self.path, 'x')
+            file.close()
 
     def read(self):
         with (open(self.path, "r")
@@ -164,5 +191,87 @@ class WoodChipperFile:
             for text_line in self.text:
                 text_file.write(text_line)
 
+    def clear(self):
+        self.text = list (())
+
+    def append_line(self, text):
+        fixedText = text
+        if fixedText[-1] != '\n':
+            fixedText = fixedText + '\n'
+        self.text.append(fixedText)
+
+    def insert_line(self, index, text):
+        fixedText = text
+        if fixedText[-1] != '\n':
+            fixedText = fixedText + '\n'
+        self.text.insert(index, fixedText)
+
+    def run_per_line(self, _func):
+        retVal = True
+        for rawLine in self.text:
+            line = rawLine
+            if line[-1] == '\n':
+                line = line[:-1]
+            retVal = retVal and _func(line)
+        return retVal
+
     def __str__(self):
         return self.name + " (" + self.path + "): " + len(self.text) + " items"
+
+class WoodchipperSettingsFile:
+    def __init__(self):
+        self.path = pathlib.Path.home() / ".wcp_Settings.txt"
+        self.file = WoodChipperFile(self.path)
+        self.keys = set(())
+        self.values = {}
+
+    def load(self):
+        self.file.read()
+        self.file.run_per_line(self._add_from_file)
+        if "debug" not in self.keys:
+            self.set_key("debug","off")
+
+    def save(self):
+        self.file.clear()
+        for key in self.keys:
+            self.file.append_line(key+":"+self.values[key])
+        self.file.write()
+
+
+    def __getitem__(self, item):
+        return self.values[item]
+
+    def __setitem__(self, key, value):
+        self.set_key(key,value)
+
+    def set_key(self, key, value):
+        self.keys.add(key)
+        self.values[key] = value
+
+    def get_key(self, key):
+        return self.values[key]
+
+    def _add_from_file(self, text):
+        linePieces = text.split(':')
+        self.set_key(linePieces[0], linePieces[1])
+        return True
+
+    def get_debug(self):
+        return self["debug"] == "on"
+
+    def flip_debug(self):
+        debug = self["debug"]
+        if debug == "off":
+            self["debug"] = "on"
+        else:
+            self["debug"] = "off"
+
+    def is_defined(self, key):
+        return key in self.keys
+
+    def get_or_default(self, key, default="undefined"):
+        if self.is_defined(key):
+            return self[key]
+        else:
+            self.set_key(key, default)
+            self.save()
